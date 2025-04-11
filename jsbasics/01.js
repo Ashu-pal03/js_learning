92ba05d9c1800a6fc42e824f777c29695fd01c14
Ashu-pal03 <palashutosh077@gmail.com>
2025-04-11 12:52:25 +0000

    data_types

---
+++ jsbasics/01.js
@@ -0,0 +1,11 @@
+let name="ashutosh"
+let id=123
+let account=12456
+console.table([name,id,account])
+
+//data types
+//int 2 to the power 53
+//bigint
+//string
+//null => it is a object
+//undefined =it is undefined
